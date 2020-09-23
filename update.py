60061c19e3051ac19455df1a5e101ba3b5dadb70
Marc Friedenberg <mfriedenberg@gmail.com>
2020-09-23 11:57:26 -0400

    First pass at email updates

--- update.py
+++ update.py
@@ -1,18 +1,81 @@
+#selenium imports
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
+
+#sqlite import
 import sqlite3
-#from sqlite3 import Error
+
+#sendgrid imports
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+def send_email_update():
+    message = Mail(
+        from_email='maf946@psu.edu',
+        to_emails='mfriedenberg@gmail.com',
+        subject='PSU Covid Dashboard Update',
+        html_content='New overall total positive number: ' + str(overall_total_positive))
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+    except Exception as e:
+        print(e.message)
+
+def update_database():
+    insert_string = "INSERT INTO covid_data (overall_total_positive, overall_current_active, overall_cases_no_longer_active,  overall_persons_currently_in_quarantine, overall_persons_currently_in_isolation, ondemand_total_tests_performed, ondemand_total_positive_cases, ondemand_total_awaiting_results, random_total_tests_performed, random_total_positive_cases, random_total_awaiting_results) VALUES ("
+    insert_string += str(overall_total_positive) + ", "
+    insert_string += overall_current_active + ", "
+    insert_string += overall_cases_no_longer_active + ", "
+    insert_string += overall_persons_currently_in_quarantine + ", "
+    insert_string += overall_persons_currently_in_isolation + ", "
+    insert_string += ondemand_total_tests_performed + ", "
+    insert_string += ondemand_total_positive_cases + ", "
+    insert_string += ondemand_total_awaiting_results + ", "
+    insert_string += random_total_tests_performed + ", "
+    insert_string += random_total_positive_cases + ", "
+    insert_string += random_total_awaiting_results + ");"
+    cur.execute(insert_string)
+    conn.commit()
+
+def print_updated_numbers():
+    print("## Overall Numbers")
+    print("")
+    print ("Total Positive Cases: ", overall_total_positive)
+    print ("Current Active Cases: ", overall_current_active)
+    print ("Cases No Longer Active: ", overall_cases_no_longer_active)
+    print("")
+    print ("Persons Currently in Quarantine: ", overall_persons_currently_in_quarantine)
+    print ("Persons Currently in Isolation: ", overall_persons_currently_in_isolation)
+    print("")
+    print ("## On-Demand Testing")
+    print("")
+    print ("Total Tests Performed: ", ondemand_total_tests_performed)
+    print ("Total Positive Cases: ", ondemand_total_positive_cases)
+    print ("Total Awaiting Results: ", ondemand_total_awaiting_results)
+    print("")
+    print ("## Random Testing")
+    print("")
+    print ("Total Tests Performed: ", random_total_tests_performed)
+    print ("Total Positive Cases: ", random_total_positive_cases)
+    print ("Total Awaiting Results: ", random_total_awaiting_results)
 
 def create_connection():
-    """ create a database connection to a SQLite database """
+    # create a database connection to a SQLite database
     conn = None
     try:
         conn = sqlite3.connect('psu_covid_dash_checker.sqlite3')
-        print(sqlite3.version)
     except:
         print ("error connecting to db")
     return conn
+
+# create a database connection
+conn = create_connection()
+cur = conn.cursor()
+
+query_string = "select overall_total_positive from covid_data order by update_time desc limit 1"
+last_recorded_overall_total_positive = int(cur.execute(query_string).fetchone()[0])
 
 options = Options()
 options.headless = True
@@ -33,7 +96,7 @@ for i in iframe_list:
     driver.switch_to.default_content()
 driver.quit()
 
-overall_total_positive = number_list[10]
+overall_total_positive = int(number_list[10])
 overall_current_active = number_list[12]
 overall_cases_no_longer_active = number_list[11]
 overall_persons_currently_in_quarantine = number_list[7]
@@ -45,44 +108,17 @@ random_total_tests_performed = number_list[5]
 random_total_positive_cases = number_list[4]
 random_total_awaiting_results = number_list[3]
 
-# create a database connection
-conn = create_connection()
+# overall_total_positive = overall_total_positive + 1 # DEBUG LINE, TO TEST FOR WHEN THERE ARE UPDATES
 
-cur = conn.cursor()
-insert_string = "INSERT INTO covid_data (overall_total_positive, overall_current_active, overall_cases_no_longer_active,  overall_persons_currently_in_quarantine, overall_persons_currently_in_isolation, ondemand_total_tests_performed, ondemand_total_positive_cases, ondemand_total_awaiting_results, random_total_tests_performed, random_total_positive_cases, random_total_awaiting_results) VALUES ("
-insert_string += overall_total_positive + ", "
-insert_string += overall_current_active + ", "
-insert_string += overall_cases_no_longer_active + ", "
-insert_string += overall_persons_currently_in_quarantine + ", "
-insert_string += overall_persons_currently_in_isolation + ", "
-insert_string += ondemand_total_tests_performed + ", "
-insert_string += ondemand_total_positive_cases + ", "
-insert_string += ondemand_total_awaiting_results + ", "
-insert_string += random_total_tests_performed + ", "
-insert_string += random_total_positive_cases + ", "
-insert_string += random_total_awaiting_results + ");"
+if (int(overall_total_positive) != last_recorded_overall_total_positive):
+    print("There has been a change")
+    print("Overall total positive: ", overall_total_positive)
+    print("Last recorded: ", last_recorded_overall_total_positive)
+    update_database()
+    send_email_update()
+    print_updated_numbers()
+else:
+    print ("No change in the recorded numbers")
 
-cur.execute(insert_string)
-conn.commit()
-cur.close()
 
-print("## Overall Numbers")
-print("")
-print ("Total Positive Cases: ", overall_total_positive)
-print ("Current Active Cases: ", overall_current_active)
-print ("Cases No Longer Active: ", overall_cases_no_longer_active)
-print("")
-print ("Persons Currently in Quarantine: ", overall_persons_currently_in_quarantine)
-print ("Persons Currently in Isolation: ", overall_persons_currently_in_isolation)
-print("")
-print ("## On-Demand Testing")
-print("")
-print ("Total Tests Performed: ", ondemand_total_tests_performed)
-print ("Total Positive Cases: ", ondemand_total_positive_cases)
-print ("Total Awaiting Results: ", ondemand_total_awaiting_results)
-print("")
-print ("## Random Testing")
-print("")
-print ("Total Tests Performed: ", random_total_tests_performed)
-print ("Total Positive Cases: ", random_total_positive_cases)
-print ("Total Awaiting Results: ", random_total_awaiting_results)
+
