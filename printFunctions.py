e526ee4c573f07570442099292c9b0f244c17f8b
Marc Friedenberg <mfriedenberg@gmail.com>
2020-09-29 08:36:53 -0400

    Update printFunctions.py
    Lint fix

--- printFunctions.py
+++ printFunctions.py
@@ -3,3 +3,4 @@ def print_updated_numbers(data_dictionary):
     value_width = len(str(max(data_dictionary.values())))
     for k, v in data_dictionary.items():
         print('{label: <{width1}} {value: <{width1}}'.format(label=k, value=v, width1=label_width, width2=value_width))
+
